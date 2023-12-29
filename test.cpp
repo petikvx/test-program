f4d0498f35e1aee1906017255c8ece21eaf30ccc
petikvx <petikvx@gmail.com>
2023-12-29 14:22:33 +0100

    ok

--- test.cpp
+++ test.cpp
@@ -4,12 +4,6 @@
 int main() {
     HKEY hKey;
 	
-    TCHAR windowsPath[MAX_PATH];
-    TCHAR systemPath[MAX_PATH];
-
-    DWORD windowsPathSize = GetWindowsDirectory(windowsPath, MAX_PATH);
-    DWORD systemPathSize = GetSystemDirectory(systemPath, MAX_PATH);
-
     LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Cryptography", 0, KEY_READ, &hKey);
 
     if (result == ERROR_SUCCESS) {
@@ -36,16 +30,6 @@ int main() {
         MessageBox(NULL, "Erreur lors de l'ouverture de la clé de registre", "Erreur", MB_ICONERROR);
     }
 	
-
-
-    if (windowsPathSize > 0 && systemPathSize > 0) {
-        TCHAR message[1024];
-        _stprintf(message, _T("Dossier Windows : %s\nDossier System : %s"), windowsPath, systemPath);
-
-        MessageBox(NULL, message, _T("Informations sur les dossiers"), MB_ICONINFORMATION);
-    } else {
-        MessageBox(NULL, _T("Erreur lors de l'obtention des chemins des dossiers"), _T("Erreur"), MB_ICONERROR);
-    }
 
     return 0;
 }
