52470198cde53dc82e878a75fa00ff2857bd6cf1
PetiKVX <petikvx@gmail.com>
2023-12-29 14:50:52 +0100

    up

--- script.py
+++ script.py
@@ -3,42 +3,69 @@ import os
 import sys
 from datetime import datetime
 import subprocess
+import pefile  # Assurez-vous d'avoir installé cette bibliothèque
 
 def calculate_hashes(file_path):
     md5_hash = hashlib.md5()
     sha256_hash = hashlib.sha256()
 
-    with open(file_path, "rb") as f:
-        while chunk := f.read(8192):
-            md5_hash.update(chunk)
-            sha256_hash.update(chunk)
+    try:
+        with open(file_path, "rb") as f:
+            while chunk := f.read(8192):
+                md5_hash.update(chunk)
+                sha256_hash.update(chunk)
+    except IOError as e:
+        print(f"Error opening or reading the file: {e}")
+        return None, None
 
     return md5_hash.hexdigest(), sha256_hash.hexdigest()
 
+def list_dlls_and_apis(file_path):
+    try:
+        pe = pefile.PE(file_path)
+        dll_api_mapping = {}
+
+        for entry in pe.DIRECTORY_ENTRY_IMPORT:
+            dll_name = entry.dll.decode('utf-8')
+            apis = [imp.name.decode('utf-8') if imp.name else 'UNKNOWN' for imp in entry.imports]
+            dll_api_mapping[dll_name] = apis
+
+        return dll_api_mapping
+    except Exception as e:
+        print(f"Error analyzing PE file: {e}")
+        return None
+
 def get_file_info(file_path):
     if not os.path.exists(file_path):
+        print("The specified file does not exist.")
         return None
 
     file_info = {}
-    file_info["MD5"] = calculate_hashes(file_path)[0]
-    file_info["SHA256"] = calculate_hashes(file_path)[1]
+    md5, sha256 = calculate_hashes(file_path)
+    if md5 is None or sha256 is None:
+        return None
 
-    # Convertir la date de création en format "DD/MM/YYYY HH:MM:SS"
+    file_info["MD5"] = md5
+    file_info["SHA256"] = sha256
+
     creation_time = os.path.getctime(file_path)
-    file_info["Date de création"] = datetime.fromtimestamp(creation_time).strftime("%d/%m/%Y %H:%M:%S")
+    file_info["Creation Date"] = datetime.fromtimestamp(creation_time).strftime("%d/%m/%Y %H:%M:%S")
 
-    # Utiliser la commande "file" pour obtenir le type de fichier
     try:
-        file_type = subprocess.check_output(["file", file_path], universal_newlines=True)
-        file_info["Type de fichier"] = file_type.strip()
+        file_type = subprocess.check_output(["file", "--brief", file_path], universal_newlines=True)
+        file_info["File Type"] = file_type.strip()
+        if "MS Windows" in file_type:
+            dll_api_mapping = list_dlls_and_apis(file_path)
+            if dll_api_mapping is not None:
+                file_info["DLLs with APIs"] = dll_api_mapping
     except subprocess.CalledProcessError:
-        file_info["Type de fichier"] = "N/A"
+        file_info["File Type"] = "N/A"
 
     return file_info
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Utilisation: python script.py nom_du_fichier.exe")
+        print("Usage: python script.py filename")
         sys.exit(1)
 
     file_path = sys.argv[1]
@@ -46,6 +73,13 @@ if __name__ == "__main__":
 
     if file_info:
         for key, value in file_info.items():
-            print(f"{key}: {value}")
+            if key == "DLLs with APIs":
+                print(f"{key}:")
+                for dll, apis in value.items():
+                    print(f"  {dll}:")
+                    for api in apis:
+                        print(f"    {api}")
+            else:
+                print(f"{key}: {value}")
     else:
-        print("Le fichier n'existe pas.")
+        print("Unable to retrieve file information.")
